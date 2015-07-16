76c407190e5ed91f31813b8db6f27073c16f2a64
Simon Pedersen <mail@sjkp.dk>
2015-07-16 23:09:38 +0200

    initial

---
+++ Delegate.SPA.Template/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿namespace $safeprojectname$.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    
+
+    public class ValuesController : ApiController
+    {
+        public async Task<IHttpActionResult> GetValues()
+        {
+            return Ok(new[] { "a", "b", "c" });
+        }
+    }
+}
